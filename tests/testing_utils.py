2cc065366a7b9ce80019e6fbe078c9b1eb1fe3b8
Jacques857 <jleongsi@ualberta.ca>
2022-01-29 10:40:29 -0700

    removed JSON requirement from GET coffee that doesn't exist

--- tests/testing_utils.py
+++ tests/testing_utils.py
@@ -1,10 +1,5 @@
 # testing_utils.py
 
-def coffee_deleted(response, id):
-    """Make sure that the given coffee is deleted
-    """
-    assert not response.json().get("id") == id
-
 def at_least_one_coffee(response):
     """Make sure that there is at least one coffee in the DB
     """
