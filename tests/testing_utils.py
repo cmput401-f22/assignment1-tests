7d33b24e8b6297033d58fe128b2b551064f320da
aaronskiba <71047780+aaronskiba@users.noreply.github.com>
2022-09-13 11:12:28 -0600

    Re-factoring Coffee to Set
    Change "coffee" to "set"
    Change "Coffee" to "Set"

--- tests/testing_utils.py
+++ tests/testing_utils.py
@@ -1,6 +1,6 @@
 # testing_utils.py
 
-def at_least_one_coffee(response):
-    """Make sure that there is at least one coffee in the DB
+def at_least_one_set(response):
+    """Make sure that there is at least one set in the DB
     """
     assert response.json().get("total") > 0
